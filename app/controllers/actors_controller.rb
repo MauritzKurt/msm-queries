79abcb6748c247cbc8297c14607233644b23b196
Kurt Mauritz <38671493+MauritzKurt@users.noreply.github.com>
2024-06-25 16:58:15 +0000

    completed tests 27/27 - finished

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -6,12 +6,10 @@ class ActorsController < ApplicationController
   def show
     the_id = params.fetch("the_id")
 
-    matching_records = Director.where({ :id => the_id })
+    matching_records = Actor.where({ :id => the_id })
 
-    @the_director = matching_records.at(0)
+    @the_actor = matching_records.at(0)
 
     render({ :template => "actor_templates/details" })
   end
 end
-
-# 45:17 vid, just copy pasted this controller from directors_controller
